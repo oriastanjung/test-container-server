4149458a8761e76d2187335c4911a3f9f9bf3f74
oriastanjung <oriastan999@gmail.com>
2023-12-23 22:24:40 +0700

    finish

---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello world");
+});
+
+app.post("/saveFormData", (req, res) => {
+  const formData = req.body;
+  const dataString = JSON.stringify(formData);
+
+  // Save data to public folder
+  const filePath = "public/attendanceData.txt";
+
+  fs.writeFile(filePath, dataString, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    res.status(200).send("File saved successfully");
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
