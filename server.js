9912ddc729c4fc80fd712e43e8a20b4b6aebb751
oriastanjung <oriastan999@gmail.com>
2023-12-23 22:34:26 +0700

    finish

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const port = 3000;
-
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("hello world");
